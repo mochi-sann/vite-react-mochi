9cac37ff631070168c4c91648ff858acf4c65e0a
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-06-06 21:24:55 +0900

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button
+            onClick={() => setCount((count) => count + 1)}
+            className="p-2 min-w-50 bg-purple-800 duration-100 hover:bg-purple-900 focus:ring-4 rounded-lg"
+          >
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
