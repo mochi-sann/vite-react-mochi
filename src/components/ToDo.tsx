555d27685b7986582e8fdf838e58f2ee0544d62d
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-06-07 12:06:38 +0900

    色々した

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,10 +1,25 @@
 import React, { useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
+import { makeStyles } from "@material-ui/core/styles"
+import Button from "@material-ui/core/Button"
 import { TodoListGen, AddList, RemoveList } from "../store/Todolist.store"
 
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+    marginTop: "8px"
+  }
+})
 const ToDo: React.VFC = () => {
   const ToDoList = useSelector(TodoListGen)
   const [InputValue, setInputValue] = useState("")
+  const classes = useStyles()
 
   const dispatch = useDispatch()
 
@@ -20,16 +35,25 @@ const ToDo: React.VFC = () => {
           type="text"
           value={InputValue}
         />
-        <button
-          className="focus:ring-4 w-full mt-2 p-4 rounded-md  transition duration-150 focus:ring-indigo-300 bg-indigo-600	"
+        <Button
+          className={
+            classes.root +
+            " focus:ring-4 w-full  p-4 rounded-md  transition duration-150  bg-indigo-600	"
+          }
+          color="primary"
           onClick={() => {
+            if (!InputValue) {
+              return
+            }
             dispatch(AddList(InputValue))
             console.log(InputValue)
             setInputValue("")
           }}
+          size="large"
+          variant="contained"
         >
           追加
-        </button>
+        </Button>
       </div>
       {ToDoList.map((value, index) => {
         return (
