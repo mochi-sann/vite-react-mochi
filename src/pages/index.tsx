b0a679e2f750b336550084bd3cdcb469314f792f
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-07-24 19:43:41 +0900

    いろいろした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,16 @@
 import React, { useState, useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
-import { makeStyles } from "@material-ui/core/styles"
 
 import { Helmet } from "react-helmet"
-import { TextField, Button } from "@material-ui/core"
+import { SiAddthis } from "react-icons/si"
+
 import { AddList } from "../store/Todolist.store"
 import { SetAppBarName, TitleGen } from "../store/Title.store"
 import ToDoLists from "../components/ToDoLists"
 
-const useStyles = makeStyles({
-  root: {
-    background: "linear-gradient( 135deg, #52E5E7 10%, #130CB7 100%)",
-    border: 0,
-    borderRadius: 3,
-    boxShadow: "0 3px 5px 2px #130CB766",
-    // color: "white",
-    height: 48,
-    padding: "0 30px",
-    marginTop: "20px"
-  }
-})
 const ToDo: React.VFC = () => {
   const SiteTitle = useSelector(TitleGen)
   const [InputValue, setInputValue] = useState("")
-  // const classes = useStyles()
 
   const dispatch = useDispatch()
   useEffect(() => {
@@ -37,21 +24,19 @@ const ToDo: React.VFC = () => {
         <title>{SiteTitle.Name || "Todoりすと"}</title>
       </Helmet>
 
-      <div>
+      <div className="flex space-x-4">
         <input
-          className=" flex-1 appearance-none border border-transparent w-full py-2 px-4 bg-white text-gray-900 placeholder-gray-400 shadow-md rounded-lg text-base focus:outline-none focus:ring-4 focus:ring-indigo-300  focus:border-transparent ring-2	ring-gray-800 transition  duration-200"
+          className=" flex-1 appearance-none border border-transparent w-full py-2 px-4 bg-white text-gray-900 placeholder-gray-400 shadow-md rounded-lg text-base focus:outline-none focus:ring-4 focus:ring-indigo-300  focus:border-transparent 	border border-gray-400 transition  duration-200"
           id="outlined-basic "
-          // label="new TODO"
           onChange={(e) => {
             setInputValue(e.target.value)
           }}
+          placeholder="TODOリストを追加"
           value={InputValue}
-          // variant="outlined"
         />
 
         <button
-          className="mt-4 focus:ring-4 w-full  p-4 rounded-md  transition duration-150  bg-green-500		"
-          color="primary"
+          className="bg-white transition text-gray-800 font-bold rounded border-b-2 border-green-500 hover:border-green-600 hover:bg-green-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
           onClick={() => {
             if (!InputValue) {
               return
@@ -61,7 +46,8 @@ const ToDo: React.VFC = () => {
             setInputValue("")
           }}
         >
-          追加
+          <span className="mr-2">add</span>
+          <SiAddthis />
         </button>
       </div>
       <ToDoLists />
